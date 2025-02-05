881f8b57580217f4d29a2682a22cc22fe68741d3
MikiLewy <m.lewandowski.0231@gmail.com>
2025-02-05 23:07:52 +0100

    fix: added missing suspense

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
+
 import VerifyToken from '@/features/auth/components/organisms/verify-token';
 
 const VerifyEmailPage = async () => {
-  return <VerifyToken />;
+  return (
+    <Suspense>
+      <VerifyToken />
+    </Suspense>
+  );
 };
 
 export default VerifyEmailPage;
