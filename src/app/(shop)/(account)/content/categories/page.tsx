2ad7c8ed06a4b43f19544945f4b6916cef1a2a9f
Mikolaj <m.lewandowski.0231@gmail.com>
2025-02-16 20:26:33 +0100

    fix: added metadata object to content categories and content products pages

--- src/app/(shop)/(account)/content/categories/page.tsx
+++ src/app/(shop)/(account)/content/categories/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import Categories from '@/features/account/categories/components/templates/categories';
+
+export const metadata: Metadata = {
+  title: 'Categories',
+  description: 'Manage your products categories.',
+};
 
 const CategoriesPage = async () => {
   return <Categories />;
--- src/app/(shop)/(account)/content/products/page.tsx
+++ src/app/(shop)/(account)/content/products/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import Products from '@/features/account/products/components/templates/products';
+
+export const metadata: Metadata = {
+  title: 'Products',
+  description: 'Manage your products and variants.',
+};
 
 const ProductsPage = async () => {
   return <Products />;
