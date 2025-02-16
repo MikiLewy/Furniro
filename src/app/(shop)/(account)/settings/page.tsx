fb368883fcfa9f206b5182ef7ad034923ba129b1
Mikolaj <m.lewandowski.0231@gmail.com>
2025-02-16 20:22:49 +0100

    feat: added metadata object to pages

--- src/app/(shop)/(account)/settings/page.tsx
+++ src/app/(shop)/(account)/settings/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import Settings from '@/features/account/settings/components/templates/settings';
+
+export const metadata: Metadata = {
+  title: 'Settings',
+  description: 'Manage your account settings and preferences.',
+};
 
 const SettingsPage = async () => {
   return <Settings />;
